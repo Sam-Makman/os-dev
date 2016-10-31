a3d112c865bb29a1403a9e419ca98d2408b1994a
Sam Makman <sammakman@gmail.com>
2016-10-31 11:20:05 -0700

    now in 32 bit mode

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,39 +1,28 @@
+; A boot  sector  that  enters 32-bit  protected  mode.
 [org 0x7c00]
+mov bp, 0x9000          ; Set  the  stack.
+mov sp, bp
+mov bx, MSG_REAL_MODE
+call  print_string
+call  switch_to_pm      ; Note  that we  never  return  from  here.
+jmp $
 
-    mov [BOOT_DRIVE], dl ; BIOS stores out boot drive in dl,
-                         ; store this value so we don't lose it
+%include "./print/print_string.asm"
+%include "gdt.asm"
+%include "./print/print_string_pm.asm"
+%include "switch_to_pm.asm"
+[bits  32]
 
-    mov bp, 0x8000 ; this is the start of our stack, way out of the way
-    mov sp, bp      ; set stack pointer to base of the stack
+; This is  where we  arrive  after  switching  to and  initialising  protected  mode.
+BEGIN_PM:
+  mov ebx , MSG_PROT_MODE
+  call  print_string_pm      ; Use  our 32-bit  print  routine.
+  jmp $                      ; Hang.
 
-    mov bx, 0x9000 ; load 5 sectors 0x0000 (ES) to 0x9000 (BX)
-    mov dh, 2       ; from boot disk
-    mov dl, [BOOT_DRIVE]
-    call disk_load
+; Global  variables
+MSG_REAL_MODE   db "Started  in 16-bit  Real  Mode", 0
+MSG_PROT_MODE   db "Successfully  landed  in 32-bit  Protected  Mode", 0
 
-    mov dx, [0x9000] ; print the first loaded work, which
-    call print_hex ; we expect to be 0xdada. Stored at 0x9000
-
-    mov dx, [0x9000 + 512] ; print the first word from the second sector
-    call print_hex ; we expect 0xface
-
-    jmp $
-
-    %include "./print/print_string.asm"
-    %include "./hex/print_hex.asm"
-    %include "disk_load.asm"
-
-
-    ;Global Variables
-    BOOT_DRIVE db 0
-
-    ;bootsector padding
-    times 510-($-$$) db 0
-    dw 0xaa55
-
-;We know  that  BIOS  will  load  only  the  first 512-byte  sector  from  the disk ,
-; so if we  purposely  add a few  more  sectors  to our  code by  repeating  some
-; familiar  numbers , we can  prove  to  ourselfs  that we  actually  loaded  those
-; additional  two  sectors  from  the  disk we  booted  from.
-times  256 dw 0xDADA
-times  256 dw 0xFACE
+; Bootsector  padding
+times  510-($-$$) db 0
+dw 0xaa55
